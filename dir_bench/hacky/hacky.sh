2c79f6c89af423d07a0dfdded5dc29b793d6e053
FutureApp <Michael Czaja>
2019-04-26 20:42:02 +0200

    Deletion of hacky-files

--- dir_bench/hacky/hacky.sh
+++
@@ -1,3 +0,0 @@
-kubectl exec -t thadoop-hadoop-hdfs-nn-0 -- bash -c "mkdir -p /export; cd /export; \
-                                                    hdfs dfs -get /. /export"; \
-kubectl cp thadoop-hadoop-hdfs-nn-0:/export .                                                     
