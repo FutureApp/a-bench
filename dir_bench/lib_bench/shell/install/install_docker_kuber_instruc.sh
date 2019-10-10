451c1ac38a8253f2d953c8d4946b569a2b5fd7bb
FutureApp <Michael Czaja>
2019-10-10 16:26:52 +0200

    Install update

--- dir_bench/lib_bench/shell/install/install_docker_kuber_instruc.sh
+++ dir_bench/lib_bench/shell/install/install_docker_kuber_instruc.sh
@@ -9,7 +9,7 @@
 ### Install packages to allow apt to use a repository over HTTPS
     apt-get update && apt-get install apt-transport-https ca-certificates curl software-properties-common
 
-### Add Docker’s official GPG key
+### Add Dockers official GPG key
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
 ### Add docker apt repository.
---
+++ dir_bench/lib_bench/shell/install/install_git.sh
@@ -0,0 +1,4 @@
+
+sudo add-apt-repository -y multiverse
+sudo apt-get update
+sudo apt-get -y install git;
