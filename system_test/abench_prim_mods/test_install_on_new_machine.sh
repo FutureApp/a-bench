4389b16ab4dd5e889aed6e2bfed15a3977705792
FutureApp <Michael Czaja>
2019-10-10 16:33:38 +0200

    Remove && in script

--- system_test/abench_prim_mods/test_install_on_new_machine.sh
+++ system_test/abench_prim_mods/test_install_on_new_machine.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 echo "Installationsprozess der ABench - Infrastruktur. Neue Umgebung-Test" &&\
-start=$(date +%s)&&\
-sudo apt-get update &&\
-sudo apt-get install -y git && \
-rm -fr ~/wd/abench;
-mkdir -p ~/wd/abench &&
-cd ./wd/abench &&
-git clone https://github.com/FutureApp/a-bench.git && \
-cd a-bench && chmod +x admin.sh &&\
-bash admin.sh auto_install && \
-end_install=(date +%s) &&\
-bash admin.sh senv_a &&\
-end_run=(date +%s) &&\
-install_runtime=$((end_install-start)) &&\
-total_runtime=$((end_run-start)) &&\
+start=$(date +%s)
+sudo apt-get update
+sudo apt-get install -y git
+rm -fr ~/wd/abench
+mkdir -p ~/wd/abench
+cd ./wd/abench
+git clone https://github.com/FutureApp/a-bench.git
+cd a-bench && chmod +x admin.sh
+bash admin.sh auto_install
+end_install=(date +%s)
+bash admin.sh senv_a
+end_run=(date +%s)
+install_runtime=$((end_install-start))
+total_runtime=$((end_run-start))
 echo "Total execution time: $total_runtime s | Install time: $install_runtime"
