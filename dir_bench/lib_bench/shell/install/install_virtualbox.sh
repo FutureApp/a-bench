4aa4838e24911a309f1937636b9437136fa0afd4
FutureApp <Michael Czaja>
2019-10-10 14:42:43 +0200

    Bugfix VirtualBox  could be installed again.

--- dir_bench/lib_bench/shell/install/install_virtualbox.sh
+++ dir_bench/lib_bench/shell/install/install_virtualbox.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+# https://vitux.com/how-to-install-virtualbox-on-ubuntu/
+sudo add-apt-repository -y multiverse && sudo apt-get update
 sudo apt-get -y install virtualbox virtualbox-qt virtualbox-dkms 
