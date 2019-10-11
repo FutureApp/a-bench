3d558610c79a66da16b5e1786fb47472c80f099f
FutureApp <Michael Czaja>
2019-10-11 03:03:56 +0200

    Neue Umgebung-Test init

--- system_test/abench_prim_mods/test_install_on_new_machine.sh
+++ system_test/abench_prim_mods/test_install_on_new_machine.sh
@@ -14,4 +14,33 @@ bash admin.sh senv_a
 end_run=(date +%s)
 install_runtime=$((end_install-start))
 total_runtime=$((end_run-start))
-echo "Total execution time: $total_runtime s | Install time: $install_runtime"
+
+
+# Checks if all pods are available which are expected.
+# Based on
+# https://unix.stackexchange.com/questions/428614/take-output-from-grep-and-select-parts-for-variables
+podList=(   etcd-minikube heapster influxdb-client influxdb-grafana kube-addon-manager-minikube 
+            kube-apiserver-minikube kube-controller-manager-minikube kube-dns kube-proxy kube-scheduler-minikube
+            kubernetes-dashboard storage-provisioner tiller-deploy )
+searchWord="Running"
+countFailur=0
+for pod in "${podList[@]}"
+do
+    var=( $(kubectl get pods -o wide --all-namespaces | grep $pod) )
+    if [  "${var[3]}" == $searchWord ]; then
+        echo "[x] $pod is runnning."
+    else
+        echo "[ ] $pod is not runnning."
+        countFailur=$((var+1))
+    fi
+done
+
+if [ "$countFailur" -eq "0" ]; then
+   echo "Test successfully";
+   echo "Total execution time: $total_runtime s | Install time: $install_runtime"
+   exit;
+else
+    echo "Error, test failed";
+    echo "Total execution time: $total_runtime s | Install time: $install_runtime"
+    exit 1;
+fi
