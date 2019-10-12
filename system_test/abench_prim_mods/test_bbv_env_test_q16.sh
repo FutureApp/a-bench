5b14db960a57700dd659ee86345ce16731f12dfd
FutureApp <Michael Czaja>
2019-10-12 22:36:35 +0200

    Bigfix

--- system_test/abench_prim_mods/test_bbv_env_test_q16.sh
+++ system_test/abench_prim_mods/test_bbv_env_test_q16.sh
@@ -43,7 +43,7 @@ if grep -q $searchString $filePathLog_Spark; then
     echo "Found search-string (in spark): $searchString"
 else
     echo "Could not found search-string (in spark): $searchString"
-    ((countFailur++)
+    ((countFailur++))
 fi
 
 
